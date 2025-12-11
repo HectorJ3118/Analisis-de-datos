35413442a3d68df8e6615ed5074dc99059c48623
HectorJ Jimenez <jimenez.martinez.hectoryael@gmail.com>
2025-12-11 16:34:30 -0600

    11/12/2025

---
+++ src/diferencias_divididas.py
@@ -0,0 +1,48 @@
+class diferencias_divididas:
+    def __init__(self,m,x,fx,n,punto):
+        self.m=m
+        self.x=x
+        self.fx=fx
+        self.n=n
+        self.tabla=[]
+        self.punto=punto
+
+    def crear_tabla(self):
+        self.tabla.append(self.fx)
+        for i in range(1,self.m):
+            fila=[]
+            for j in range (self.m-i):
+                numerador=self.tabla[i-1][j+1]-self.tabla[i-1][j]
+                denominador=self.x[j+i]-self.x[j]
+                fila.append[numerador/denominador]
+            self.tabla.append(fila)
+        return self.tabla         
+
+    def evaluar_punto(self):
+        resultado=self.tabla[0,0]
+        prod=1.0
+        
+        for i in range(1,self.n+1):
+            prod*=(self.punto-self.x[i-1])
+            resultado+= self.tabla[i][0]*prod
+        return resultado    
+
+    
+print('INTERPOLACION POR DIFERENCIAS DIVIDIDAS DE NEWTON')
+m=int(input('Ingrese la cantidad de puntos'))
+print('Ingrese los valores de xi')
+x=[float(input(f'x{i}: '))for i in range(m)]
+print('ingrese los valores de f(xi)')
+fx=[float(input(f'f(x{i}): '))for i in range(m)]
+        
+n=int(input('Ingrese el grado de interpolacion n < =m-1'))
+if n<= m-1:
+    punto=float(input("Ingrese el punto donde quiere evaluar la funcion"))
+    metodo=diferencias_divididas(m,x,fx,n,punto)
+    metodo.crear_tabla()
+    metodo.evaluar_punto()
+else:
+    print('el grado de interpolacion debe de ser al menos uno menos que el numero de datos')    
+
+
+        
